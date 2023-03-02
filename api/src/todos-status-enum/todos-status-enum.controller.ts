22d3e34a0f48b554681a885e2583fcce58c07d8d
Oleh Tsybulskyi <darkraven987@gmail.com>
2023-03-02 15:28:59 +0200

    API: implemented guard call to protect controllers by calling AUTH microservice for Authorization data validation

--- api/src/todos-status-enum/todos-status-enum.controller.ts
+++ api/src/todos-status-enum/todos-status-enum.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/validateToken.guard';
 import { TodosStatusEnumService } from './todos-status-enum.service';
 
+@UseGuards(AuthGuard)
 @Controller('todos-status-enum')
 export class TodosStatusEnumController {
   constructor(
