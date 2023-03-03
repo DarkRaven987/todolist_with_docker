d285b16237f99f7c0dedabad621de9fb616a3cda
Oleh Tsybulskyi <darkraven987@gmail.com>
2023-03-03 13:57:57 +0200

    API & AUTH: enabled cors politicy

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -18,6 +18,8 @@ async function bootstrap() {
     },
   });
 
+  app.enableCors();
+
   await app.startAllMicroservices();
   await app.listen(port);
   Logger.log(`AUTH microservice is running on port ${microservicePort}`);
