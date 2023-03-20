e97d9109e57fba68270046b7268baf355e492015
Oleh Tsybulskyi <darkraven987@gmail.com>
2023-03-20 14:46:46 +0200

    AUTH: endpoints name refactor + using guards globally for whole controller instead of using it for each endpoint ceparately

--- auth/src/users/users.controller.ts
+++ auth/src/users/users.controller.ts
@@ -15,36 +15,32 @@ import { updateUserSchema } from './dtos/users.joi';
 import { AccessTokenGuard } from 'src/auth/guards/accessToken.guard';
 import { JoiValidationPipe } from 'src/pipes/joi.pipe';
 
+@UseGuards(AccessTokenGuard)
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @UseGuards(AccessTokenGuard)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @UseGuards(AccessTokenGuard)
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
-  @UseGuards(AccessTokenGuard)
   @Get(':id')
   findById(@Param('id') id: number) {
     return this.usersService.findById(id);
   }
 
-  @UseGuards(AccessTokenGuard)
   @UsePipes(new JoiValidationPipe(updateUserSchema))
   @Patch()
   update(@Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(updateUserDto);
   }
 
-  @UseGuards(AccessTokenGuard)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.usersService.remove(id);
